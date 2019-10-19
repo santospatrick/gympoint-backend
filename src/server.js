1ed00ad634bffe1a30a34f23d653aaf7e930a48b
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-19 17:21:40 -0300

    adiciona eslint + prettier + editorconfig

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const server = express()
+const server = express();
 
 server.get('/', (_req, res) => {
-    return res.json({message: 'maneiro mesmo!'})
-})
+  return res.json({ message: 'maneiro mesmo!' });
+});
 
-server.listen(3000)
+server.listen(3000);
