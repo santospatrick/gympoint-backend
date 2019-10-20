95b7e5808fccd4b6e7fbb86f19a94f1fe1e8f823
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-19 23:08:57 -0300

    adiciona cadastro de estudantes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,7 +21,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ error: 'User already exists' });
     }
 
     const { id, name, email } = await User.create(req.body);
