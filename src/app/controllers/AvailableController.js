6e0ccd90186a24ca29c3382c1234729ad3e01801
Patrick Santos <patrickspteixeira@gmail.com>
2019-11-16 16:15:35 -0300

    adiciona nome de estudante nos pedidos de auxilio

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,10 +1,12 @@
 import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
 
 class AvailableController {
   async index(req, res) {
     const helpOrders = await HelpOrder.findAll({
       where: { answer: null },
       attributes: ['id', 'question'],
+      include: [{ model: Student, as: 'student', attributes: ['name'] }],
     });
 
     return res.json(helpOrders);
